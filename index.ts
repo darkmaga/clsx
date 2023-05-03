03b34c791ca8faee9b0b27b17edf813977623812
Magomed Chabaev <magomed.chabaev99@gmail.com>
2023-05-03 23:38:18 +0300

    add file index

---
+++ index.ts
@@ -0,0 +1,36 @@
+const cn = (...args: any): string => {
+  let resultString: string = ''
+
+  for (let currentArgument of args) {
+    if (typeof currentArgument === 'string') {
+      resultString += `${resultString === '' ? '' : ' '}${currentArgument}`
+
+      continue
+    }
+
+    if (
+      typeof currentArgument === 'object' &&
+      currentArgument !== null &&
+      !Array.isArray(currentArgument)
+    ) {
+      for (let objectKey of Object.keys(currentArgument)) {
+        if (!currentArgument[objectKey]) continue
+
+        resultString += `${resultString === '' ? '' : ' '}${objectKey}`
+      }
+    }
+    if (Array.isArray(currentArgument)) {
+      for (let arrayElement of currentArgument) {
+        if (!arrayElement) continue
+
+        const nextString = cn(arrayElement)
+
+        if (!nextString) continue
+
+        resultString += `${resultString === '' ? '' : ' '}${nextString}`
+      }
+    }
+  }
+
+  return resultString
+}
